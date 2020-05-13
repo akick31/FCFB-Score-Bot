e7cda3a3b1cfaff98ebc3c67cb4de8b4ab1ceb82
akick31 <apkick@iastate.edu>
2020-05-12 23:29:54 -0400

    fix postseason

--- FCFBScoreBot.py
+++ FCFBScoreBot.py
@@ -1121,6 +1121,13 @@ def searchForScoreGamethread(submission, homeTeam, awayTeam, season, request, po
        or submission.link_flair_text == "Week 10 Game Thread"):
         away = parseAwayTeam(submission.selftext).lower()
         home = parseHomeTeam(submission.selftext).lower()
+        print(submission.title)
+        print(home)
+        print(away)
+        print(homeTeam)
+        print(awayTeam)
+        print(month + " " + day + " " + year)
+        print()
     # If looking for season 4...
     if ((submission.link_flair_text == "Game Thread" or submission.link_flair_text == "Week 10 Game Thread") and season == "S4" 
         and ((year == 2020 and month == 3 and day >= 20) or (year == 2020 and month > 3))
