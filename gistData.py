f6bf07c8c49059054a5129e926fbaa8bf9fee121
akick31 <apkick@iastate.edu>
2020-05-14 00:44:51 -0400

    fix plots

--- gistData.py
+++ gistData.py
@@ -241,39 +241,12 @@ def iterateThroughGistDataGameOver(hometeam, awayteam, homeVegasOdds, awayVegasO
                     curAwayWinProbability = 100 - curHomeWinProbability
                     homeWinProbability.append(curHomeWinProbability)
                     awayWinProbability.append(curAwayWinProbability)
-                    print(curHomeWinProbability)
-                    print(curAwayWinProbability)
-                    print()
                 if(row[5] == "away"):
                     curAwayWinProbability = calculateWinProbabilityGist(expectedPoints, quarter, time, int(row[1]), int(row[0]), down, distance, yardLine, playType, awayVegasOdds) * 100
                     curHomeWinProbability = 100 - curAwayWinProbability
                     awayWinProbability.append(curAwayWinProbability)
                     homeWinProbability.append(curHomeWinProbability)
-                    print(curHomeWinProbability)
-                    print(curAwayWinProbability)
-                    print()
-                
-                """# Parse the win probability
-                if((row[5] == "home" and (row[14] != "TURNOVER" and row[14] != "KICK" and row[14] != "PUNT" and row[14] != "TOUCHBACK")) 
-                   or (row[5] == "away" and (row[14] == "TURNOVER" or row[14] == "KICK" or row[14] == "PUNT" or row[14] == "TOUCHBACK"))):
-                    expectedPoints = calculateExpectedPoints(down, distance, yardLine, playType)
-                    curHomeWinProbability = calculateWinProbabilityGist(expectedPoints, quarter, time, int(row[0]), int(row[1]), down, distance, yardLine, playType, homeVegasOdds) * 100
-                    curAwayWinProbability = 100-curHomeWinProbability
-                    homeWinProbability.append(curHomeWinProbability)
-                    awayWinProbability.append(curAwayWinProbability)
-                    print(curHomeWinProbability)
-                    print(curAwayWinProbability)
-                    print()
-                elif((row[5] == "away" and (row[14] != "TURNOVER" and row[14] != "KICK" and row[14] != "PUNT" and row[14] != "TOUCHBACK")) 
-                   or (row[5] == "home" and (row[14] == "TURNOVER" or row[14] == "KICK" or row[14] == "PUNT" or row[14] == "TOUCHBACK"))):
-                    expectedPoints = calculateExpectedPoints(down, distance, yardLine, playType)
-                    curAwayWinProbability = calculateWinProbabilityGist(expectedPoints, quarter, time, int(row[1]), int(row[0]), down, distance, yardLine, playType, homeVegasOdds) * 100
-                    curHomeWinProbability = 100-curAwayWinProbability
-                    awayWinProbability.append(curAwayWinProbability)
-                    homeWinProbability.append(curHomeWinProbability)
-                    print(curHomeWinProbability)
-                    print(curAwayWinProbability)
-                    print()"""
+
             # Handle OT so that winner is at 100%
             if(rowCount == currentRow and OTFlag == 1):
                 if(curHomeScore > curAwayScore):
