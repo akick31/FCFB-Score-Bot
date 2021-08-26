b563f562b9eb0824ad245efc55da9a62f44fa28c
Andrew <apkicklighter@gmail.com>
2021-08-26 03:54:10 -0500

    overhaul win probability and charts, clean up code

--- reddit_login.py
+++ reddit_login.py
@@ -10,7 +10,7 @@ Created on Wed May 13 19:43:26 2020
 
 
 # Login to Reddit
-def loginReddit():
+def login_reddit():
     r = praw.Reddit(user_agent='ScoreboardBot by /u/akick31',
                     client_id='mbxqsh9-BGzlow',
                     client_secret='DUh4hWoT3QvsJnk3ctepVjxbDWo',
